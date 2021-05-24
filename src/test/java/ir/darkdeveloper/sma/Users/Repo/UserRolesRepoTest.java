5a7d0649e8492f1ac96e42552bc7ed9ece14a367
DarkDeveloper-Plant <goldeagle00110011@outlook.com>
2021-05-24 10:50:26 +0430

    00:03:03

---
+++ src/test/java/ir/darkdeveloper/sma/Users/Repo/UserRolesRepoTest.java
@@ -0,0 +1,11 @@
+package ir.darkdeveloper.sma.Users.Repo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class UserRolesRepoTest {
+
+    @Test
+    void getUSER() {
+    }
+}

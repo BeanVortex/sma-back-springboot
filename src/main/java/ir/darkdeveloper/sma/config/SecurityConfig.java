bde44d57431f8f4b4c8b8333e2e7d48c12cf7576
DarkDeveloper_Plant <goldeagle00110011@outlook.com>
2022-07-20 11:06:24 +0430

    dep update

--- src/main/java/ir/darkdeveloper/sma/config/SecurityConfig.java
+++ src/main/java/ir/darkdeveloper/sma/config/SecurityConfig.java
@@ -1,13 +1,11 @@
 package ir.darkdeveloper.sma.config;
 
-import ir.darkdeveloper.sma.service.UserService;
 import ir.darkdeveloper.sma.utils.JwtFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
